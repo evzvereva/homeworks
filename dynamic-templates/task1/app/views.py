e81764c470146b407f039a4885db9b8d614d20a5
Ekaterina Zvereva <tcshcc07@gmail.com>
2021-01-24 00:47:30 +0500

    homework is done

--- dynamic-templates/task1/app/views.py
+++ dynamic-templates/task1/app/views.py
@@ -1,10 +1,23 @@
+import os
+
+
+from django.conf import settings
 from django.shortcuts import render
+import csv
+
+csv_file = os.path.join(settings.BASE_DIR, 'inflation_russia.csv')
+
+
 
 def inflation_view(request):
     template_name = 'inflation.html'
-
-    # чтение csv-файла и заполнение контекста
     context = {}
+    # чтение csv-файла и заполнение контекста
+    with open(csv_file, encoding='utf-8') as csvfile:
+        file_reader = csv.DictReader(csvfile, delimiter=";")
+        list_dict = list(file_reader)
+        context['table'] = list_dict
+    print(context)
 
     return render(request, template_name,
                   context)
